135faf0ff7d692245a086856ab192ecb272603d7
Johan Mabille <johan.mabille@gmail.com>
2024-02-07 16:44:37 +0100

    Project skeleton

---
+++ include/xparrow/xparrow_version.hpp
@@ -0,0 +1,15 @@
+/***************************************************************************
+* Copyright (c) QuantStack                                                 *
+*                                                                          *
+* Distributed under the terms of the BSD 3-Clause License.                 *
+*                                                                          *
+* The full license is in the file LICENSE, distributed with this software. *
+***************************************************************************/
+#ifndef XPARROW_VERSION_HPP
+#define XPARROW_VERSION_HPP
+
+#define XPARROW_VERSION_MAJOR 0
+#define XPARROW_VERSION_MINOR 0
+#define XPARROW_VERSION_PATCH 1
+
+#endif
---
+++ test/main.cpp
@@ -0,0 +1,11 @@
+/***************************************************************************
+* Copyright (c) QuantStack                                                 *
+*                                                                          *
+* Distributed under the terms of the BSD 3-Clause License.                 *
+*                                                                          *
+* The full license is in the file LICENSE, distributed with this software. *
+***************************************************************************/
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest/doctest.h"
+
