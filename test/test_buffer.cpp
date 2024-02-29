dde159e62ec7fab9f2ecd909453b5e48b87af951
Johan Mabille <johan.mabille@gmail.com>
2024-02-29 22:47:48 +0100

    Added buffer_view class

--- test/test_buffer.cpp
+++ test/test_buffer.cpp
@@ -21,6 +21,7 @@
 namespace sparrow
 {
     using buffer_test_type = buffer<uint8_t>;
+    using view_test_type = buffer_view<uint8_t>;
 
     namespace
     {
@@ -63,12 +64,12 @@ namespace sparrow
 
         TEST_CASE("copy semantic")
         {
-            const std::size_t size = 4;
+            const std::size_t size = 4u;
             buffer_test_type b1(make_test_buffer(size), size);
             buffer_test_type b2(b1);
             CHECK_EQ(b1, b2);
 
-            const std::size_t size2 = 8;
+            const std::size_t size2 = 8u;
             buffer_test_type b3(make_test_buffer(size2, 4), size2);
             b2 = b3;
             CHECK_EQ(b2, b3);
@@ -77,7 +78,7 @@ namespace sparrow
 
         TEST_CASE("move semantic")
         {
-            const std::size_t size = 4;
+            const std::size_t size = 4u;
             buffer_test_type b1(make_test_buffer(size), size);
             buffer_test_type control(b1);
             buffer_test_type b2(std::move(b1));
@@ -86,7 +87,7 @@ namespace sparrow
             CHECK(b1.empty());
             CHECK_EQ(b1.data(), nullptr);
 
-            const std::size_t size2 = 8;
+            const std::size_t size2 = 8u;
             buffer_test_type b4(make_test_buffer(size2, 4), size2);
             buffer_test_type control2(b4);
             b2 = std::move(b4);
@@ -96,7 +97,7 @@ namespace sparrow
 
         TEST_CASE("data")
         {
-            const std::size_t size = 4;
+            const std::size_t size = 4u;
             buffer_test_type b1(make_test_buffer(size), size);
             
             const uint8_t expected_value = 101;
@@ -111,7 +112,7 @@ namespace sparrow
 
         TEST_CASE("equality comparison")
         {
-            const std::size_t size = 4;
+            const std::size_t size = 4u;
             buffer_test_type b1(make_test_buffer(size), size);
             buffer_test_type b2(make_test_buffer(size), size);
             CHECK(b1 == b2);
@@ -145,6 +146,125 @@ namespace sparrow
             b.resize(size2);
             CHECK_EQ(b.size(), size2);
             CHECK_EQ(b.data()[2], 2);
+        }
+    }
+
+    TEST_SUITE("buffer_view")
+    {
+        TEST_CASE("constructors")
+        {
+            {
+                const std::size_t size = 8u;
+                uint8_t* mem = make_test_buffer(size);
+                [[maybe_unused]] view_test_type v(mem, size);
+            }
+            
+            {
+                
+                const std::size_t size = 8u;
+                buffer_test_type b(make_test_buffer(size), size);
+                [[maybe_unused]] view_test_type v(b);
+            }
+
+            {
+                const std::size_t size = 8u;
+                uint8_t* mem = make_test_buffer(size);
+                view_test_type v(mem, size);
+
+                CHECK_EQ(v.data(), mem);
+                CHECK_EQ(v.size(), size);
+                CHECK_EQ(v.data()[2], uint8_t(2));
+            }
+
+            {
+                const std::size_t size = 8u;
+                buffer_test_type b(make_test_buffer(size), size);
+                view_test_type v(b);
+
+                CHECK_EQ(v.data(), b.data());
+                CHECK_EQ(v.size(), b.size());
+                CHECK_EQ(v.data()[2], uint8_t(2));
+            }
+        }
+
+        TEST_CASE("copy semantic")
+        {
+            const std::size_t size = 4u;
+            buffer_test_type b(make_test_buffer(size), size);
+            view_test_type v1(b);
+            view_test_type v2(v1);
+            CHECK_EQ(v1, v2);
+
+            const std::size_t size2 = 8u;
+            buffer_test_type b2(make_test_buffer(size2, 4), size2);
+            view_test_type v3(b2);
+            v2 = v3;
+            CHECK_EQ(v2, v3);
+            CHECK_NE(v1, v3);
+        }
+
+        TEST_CASE("move semantic")
+        {
+            const std::size_t size = 4u;
+            buffer_test_type b(make_test_buffer(size), size);
+            view_test_type v1(b);
+            view_test_type v2(std::move(v1));
+            CHECK_EQ(v1, v2);
+
+            const std::size_t size2 = 8u;
+            buffer_test_type b2(make_test_buffer(size2, 4), size2);
+            view_test_type v3(b2);
+            v2 = std::move(v3);
+            CHECK_EQ(v2, v3);
+        }
+
+        TEST_CASE("data")
+        {
+            const std::size_t size = 4u;
+            buffer_test_type b(make_test_buffer(size), size);
+            view_test_type v1(b);
+
+            const uint8_t expected_value = 101;
+            const std::size_t idx = 3u;
+            b.data()[idx] = expected_value;
+            CHECK_EQ(v1.data()[idx], expected_value);
+
+            view_test_type v2(v1);
+            CHECK_EQ(v2.data()[idx], expected_value);
+
+            view_test_type v3(std::move(v1));
+            CHECK_EQ(v3.data()[idx], expected_value);
+        }
+
+        TEST_CASE("equality comparison")
+        {
+            const std::size_t size = 4u;
+            buffer_test_type b1(make_test_buffer(size), size);
+            buffer_test_type b2(make_test_buffer(size), size);
+            view_test_type v1(b1), v2(b2);
+            CHECK(v1 == v2);
+
+            const std::size_t size2 = 8u;
+            buffer_test_type b3(make_test_buffer(size2), size2);
+            view_test_type v3(b3);
+            CHECK(v1 != v3);
+        }
+
+        TEST_CASE("swap")
+        {
+            const std::size_t size1 = 4u;
+            const std::size_t size2 = 8u;
+
+            buffer_test_type b1(make_test_buffer(size1), size1);
+            buffer_test_type b2(make_test_buffer(size2), size2);
+            view_test_type v1(b1), v2(b2);
+            auto* data1 = b1.data();
+            auto* data2 = b2.data();
+            v1.swap(v2);
+            CHECK_EQ(v1.size(), size2);
+            CHECK_EQ(v1.data(), data2);
+            CHECK_EQ(v2.size(), size1);
+            CHECK_EQ(v2.data(), data1);
         }
     }
 }
