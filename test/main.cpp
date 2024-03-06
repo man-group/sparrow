b6c1c3999a25c6fb67b24e64701ef700a596c241
Klaim (Joël Lamotte) <142265+Klaim@users.noreply.github.com>
2024-03-06 16:35:13 +0100

    sorry!

--- test/main.cpp
+++ test/main.cpp
@@ -25,5 +25,5 @@ TEST_CASE("version is readable")
     // We only try to make sure the version valeus are printable, whatever their type.
     // AKA this is not written to be fancy but to force conversion to string.
     using namespace sparrow;
-    [[maybe_unused]] const std::string printable_version = std::string("sparrow version : ") + std::to_string(SPARROW_VERSION_MAJOR) + "." + std::to_string(SPARROW_VERSION_MINOR) + "." std::to_string(SPARROW_VERSION_PATCH);
+    [[maybe_unused]] const std::string printable_version = std::string("sparrow version : ") + std::to_string(SPARROW_VERSION_MAJOR) + "." + std::to_string(SPARROW_VERSION_MINOR) + "." + std::to_string(SPARROW_VERSION_PATCH);
 }
