8aaaed6c659058ae18a8077f0da124630057915d
Johan Mabille <johan.mabille@gmail.com>
2024-03-15 16:09:58 +0100

    Added reference_proxy implementation (#24)

--- include/sparrow/array_data.hpp
+++ include/sparrow/array_data.hpp
@@ -14,12 +14,26 @@
 
 #pragma once
 
+#include <cassert>
+#include <compare>
+#include <concepts>
+#include <optional>
+#include <vector>
+
 #include "sparrow/buffer.hpp"
 #include "sparrow/data_type.hpp"
 #include "sparrow/dynamic_bitset.hpp"
 
 namespace sparrow
 {
+    /**
+     * Structure holding the raw data.
+     *
+     * array_data is meant to be used by the
+     * different layout classes to implement
+     * the array API, based on the type
+     * specified in the type attribute.
+     */
     struct array_data
     {
         using block_type = std::uint8_t;
@@ -36,8 +50,252 @@ namespace sparrow
         std::vector<array_data> child_data;
     };
 
-    struct null_type
+    /**
+     * Reference type used in layout classes.
+     * This class provides a similar behavior
+     * as std::optional, but as reference on data
+     * in an array_data.
+     */
+    template <class L>
+    class reference_proxy
     {
+    public:
+
+        using self_type = reference_proxy<L>;
+        using layout_type = L;
+        using value_type = typename L::inner_value_type;
+        using size_type = typename L::size_type;
+
+        reference_proxy(layout_type& l, size_type index);
+
+        bool has_value() const;
+        explicit operator bool() const;
+
+        value_type& value();
+        const value_type& value() const;
+
+        self_type& operator=(const self_type& rhs);
+        self_type& operator=(self_type&& rhs);
+        self_type& operator=(std::nullopt_t);
+
+        template <std::convertible_to<typename L::inner_value_type> U>
+        self_type& operator=(const std::optional<U>& rhs);
+
+        template <std::convertible_to<typename L::inner_value_type> U>
+        self_type& operator=(std::optional<U>&& rhs);
+
+        template <std::convertible_to<typename L::inner_value_type> U>
+        self_type& operator=(U&& value);
+
+        void swap(self_type& rhs);
+        
+    private:
+
+        void reset();
+
+        template <class U>
+        void update(U&& u);
+
+        template <class U>
+        void update_value(U&& u);
+
+        layout_type* p_layout;
+        size_type m_index;
     };
-    constexpr null_type null;
+
+    template <class L>
+    void swap(reference_proxy<L>& lhs, reference_proxy<L>& rhs);
+
+    template <class L1, class L2>
+    bool operator==(const reference_proxy<L1>& lhs, const reference_proxy<L2>& rhs);
+
+    template <class L, class T>
+    bool operator==(const reference_proxy<L>& lhs, const T& rhs);
+
+    template <class L>
+    bool operator==(const reference_proxy<L>& lhs, std::nullopt_t);
+
+    template <class L1, class L2>
+    std::strong_ordering operator<=>(const reference_proxy<L1>& lhs, const reference_proxy<L2>& rhs);
+
+    template <class L, class T>
+    std::strong_ordering operator<=>(const reference_proxy<L>& lhs, const T& rhs);
+
+    /**********************************
+     * reference_proxy implementation *
+     **********************************/
+
+    template <class L>
+    reference_proxy<L>::reference_proxy(layout_type& l, size_type index)
+        : p_layout(&l)
+        , m_index(index)
+    {
+    }
+
+    template <class L>
+    bool reference_proxy<L>::has_value() const
+    {
+        return p_layout->has_value(m_index);
+    }
+
+    template <class L>
+    reference_proxy<L>::operator bool() const
+    {
+        return has_value();
+    }
+
+    template <class L>
+    auto reference_proxy<L>::value() -> value_type&
+    {
+        assert(has_value());
+        return p_layout->value(m_index);
+    }
+
+    template <class L>
+    auto reference_proxy<L>::value() const -> const value_type&
+    {
+        assert(has_value());
+        return p_layout->value(m_index);
+    }
+
+    template <class L>
+    auto reference_proxy<L>::operator=(const self_type& rhs) -> self_type&
+    {
+        update(rhs);
+        return *this;
+    }
+
+    template <class L>
+    auto reference_proxy<L>::operator=(self_type&& rhs) -> self_type&
+    {
+        update(std::move(rhs));
+        return *this;
+    }
+
+    template <class L>
+    auto reference_proxy<L>::operator=(std::nullopt_t) -> self_type&
+    {
+        reset();
+        return *this;
+    }
+
+    template <class L>
+    template <std::convertible_to<typename L::inner_value_type> U>
+    auto reference_proxy<L>::operator=(const std::optional<U>& rhs) -> self_type& 
+    {
+        update(rhs);
+        return *this;
+    }
+
+    template <class L>
+    template <std::convertible_to<typename L::inner_value_type> U>
+    auto reference_proxy<L>::operator=(std::optional<U>&& rhs) -> self_type& 
+    {
+        update(std::move(rhs));
+        return *this;
+    }
+
+    template <class L>
+    template <std::convertible_to<typename L::inner_value_type> U>
+    auto reference_proxy<L>::operator=(U&& value) -> self_type&
+    {
+        update_value(std::forward<U>(value));
+        return *this;
+    }
+
+    template <class L>
+    void reference_proxy<L>::reset()
+    {
+        p_layout->reset(m_index);
+    }
+
+    template <class L>
+    void reference_proxy<L>::swap(self_type& rhs)
+    {
+        const bool this_has_value = has_value();
+        const bool rhs_has_value = rhs.has_value();
+        using std::swap;
+        if (this_has_value)
+        {
+            if (rhs_has_value)
+            {
+                swap(p_layout->value(m_index), rhs.p_layout->value(rhs.m_index));
+            }
+            else
+            {
+                rhs.update_value(value());
+                reset();
+            }
+        }
+        else if (rhs_has_value)
+        {
+            update_value(rhs.value());
+            rhs.reset();
+        }
+        // If none has value, nothing to do
+    }
+
+    template <class L>
+    template <class U>
+    void reference_proxy<L>::update(U&& u)
+    {
+        if (u.has_value())
+        {
+            update_value(std::forward<U>(u).value());
+        }
+        else
+        {
+            reset();
+        }
+    }
+
+    template <class L>
+    template <class U>
+    void reference_proxy<L>::update_value(U&& u)
+    {
+        p_layout->update(m_index, std::forward<U>(u));
+    }
+
+    template <class L>
+    void swap(reference_proxy<L>& lhs, reference_proxy<L>& rhs)
+    {
+        lhs.swap(rhs);
+    }
+
+    template <class L1, class L2>
+    bool operator==(const reference_proxy<L1>& lhs, const reference_proxy<L2>& rhs)
+    {
+        return (lhs && rhs && (lhs.value() == rhs.value())) || (!lhs && !rhs);
+
+    }
+
+    template <class L, class T>
+    bool operator==(const reference_proxy<L>& lhs, const T& rhs)
+    {
+        return lhs && (lhs.value() == rhs);
+    }
+
+    template <class L>
+    bool operator==(const reference_proxy<L>& lhs, std::nullopt_t)
+    {
+        return !lhs;
+    }
+
+    template <class L>
+    std::strong_ordering operator<=>(const reference_proxy<L>& lhs, const reference_proxy<L>& rhs)
+    {
+        return (lhs && rhs) ? (lhs.value() <=> rhs.value()) : (lhs.has_value() <=> rhs.has_value());
+    }
+
+    template <class L, class T>
+    std::strong_ordering operator<=>(const reference_proxy<L>& lhs, const T& rhs)
+    {
+        return lhs ? (lhs.value() <=> rhs) : std::strong_ordering::less;
+    }
+
+    template <class L>
+    std::strong_ordering operator<=>(const reference_proxy<L>& lhs, std::nullopt_t)
+    {
+        return lhs.has_value() <=> false;
+    }
 }
---
+++ test/test_array_data.cpp
@@ -0,0 +1,221 @@
+// Copyright 2024 Man Group Operations Limited
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "doctest/doctest.h"
+
+#include <initializer_list>
+#include "sparrow/array_data.hpp"
+
+namespace sparrow
+{
+    class mock_layout
+    {
+    public:
+
+        using value_type = std::optional<int>;
+        using inner_value_type = value_type::value_type;
+        using reference = reference_proxy<mock_layout>;
+        using size_type = std::size_t;
+        using storage_type = std::vector<value_type>;
+
+        mock_layout() = default;
+        mock_layout(std::initializer_list<value_type> l)
+            : m_storage(l)
+        {
+        }
+
+        reference operator[](size_type pos)
+        {
+            return reference(*this, pos);
+        }
+
+        const storage_type& storage() const
+        {
+            return m_storage;
+        }
+
+    private:
+
+        bool has_value(size_type index) const
+        {
+            return m_storage[index].has_value();
+        }
+
+        inner_value_type& value(size_type index)
+        {
+            return m_storage[index].value();
+        }
+
+        const inner_value_type& value(size_type index) const
+        {
+            return m_storage[index].value();
+        }
+
+        void reset(size_type index)
+        {
+            m_storage[index] = std::nullopt;
+        }
+
+        void update(size_type index, int v)
+        {
+            m_storage[index] = v;
+        }
+
+        storage_type m_storage;
+        friend class reference_proxy<mock_layout>;
+    };
+
+    struct ref_proxy_fixture
+    {
+        ref_proxy_fixture()
+        {
+            m_layout = {
+                std::make_optional(2),
+                std::make_optional(5),
+                std::nullopt,
+                std::make_optional(7)
+            };
+        }
+
+        int stored_value(mock_layout::size_type index) const
+        {
+            return m_layout.storage()[index].value();
+        }
+
+        mock_layout m_layout;
+    };
+
+    TEST_SUITE("reference_proxy")
+    {
+        TEST_CASE_FIXTURE(ref_proxy_fixture, "has_value")
+        {
+            auto ref0 = m_layout[0];
+            CHECK(ref0.has_value());
+            CHECK(ref0);
+
+            auto ref2 = m_layout[2];
+            CHECK(!ref2.has_value());
+            CHECK(!ref2);
+        }
+
+        TEST_CASE_FIXTURE(ref_proxy_fixture, "value")
+        {
+            auto ref0 = m_layout[0];
+            CHECK_EQ(ref0.value(), stored_value(0));
+            static_assert(std::same_as<decltype(ref0.value()), int&>);
+
+            const auto cref0 = m_layout[0];
+            CHECK_EQ(cref0.value(), stored_value(0));
+            static_assert(std::same_as<decltype(cref0.value()), const int&>);
+        }
+
+        TEST_CASE_FIXTURE(ref_proxy_fixture, "assign")
+        {
+            int expected_value0 = 4;
+            auto ref0 = m_layout[0];
+            ref0 = expected_value0;
+            CHECK_EQ(stored_value(0), expected_value0);
+
+            ref0 = std::nullopt;
+            CHECK(!ref0.has_value());
+
+            ref0 = expected_value0;
+            CHECK_EQ(stored_value(0), expected_value0);
+
+            int expected_value2 = 3;
+            auto ref2 = m_layout[2];
+            ref2 = std::make_optional(expected_value2);
+            CHECK_EQ(stored_value(2), expected_value2);
+
+            ref0 = ref2;
+            CHECK_EQ(stored_value(0), expected_value2);
+
+            int expected_value3 = 8;
+            std::optional<int> opt(expected_value3);
+            auto ref3 = m_layout[3];
+            ref3 = opt;
+            CHECK_EQ(stored_value(3), expected_value3);
+
+            ref0 = std::move(ref3);
+            CHECK_EQ(stored_value(0), expected_value3);
+        }
+
+        TEST_CASE_FIXTURE(ref_proxy_fixture, "swap")
+        {
+            int expected_value0 = stored_value(0);
+            int expected_value3 = stored_value(3);
+
+            auto ref0 = m_layout[0];
+            auto ref2 = m_layout[2];
+            auto ref3 = m_layout[3];
+
+            ref0.swap(ref2);
+            CHECK(!ref0);
+            CHECK(ref2);
+            CHECK_EQ(ref2.value(), expected_value0);
+
+            swap(ref2, ref3);
+            CHECK_EQ(ref2.value(), expected_value3);
+            CHECK_EQ(ref3.value(), expected_value0);
+        }
+
+        TEST_CASE_FIXTURE(ref_proxy_fixture, "comparison")
+        {
+            SUBCASE("equality")
+            {
+                auto ref0 = m_layout[0];
+                auto ref1 = m_layout[1];
+                auto ref2 = m_layout[2];
+
+                CHECK(ref0 != ref1);
+                CHECK(ref0 != stored_value(1));
+                CHECK(stored_value(0) != ref1);
+                ref0 = ref1;
+                CHECK(ref0 == ref1);
+                CHECK(ref0 == stored_value(1));
+                CHECK(stored_value(0) == ref1);
+
+                CHECK(ref0 != ref2);
+            }
+
+            SUBCASE("inequality")
+            {
+                auto ref0 = m_layout[0];
+                auto ref2 = m_layout[2];
+                auto ref3 = m_layout[3];
+
+                CHECK(ref2 < ref0);
+                CHECK(ref2 <= ref0);
+                CHECK(ref3 > ref2);
+                CHECK(ref3 >= ref2);
+
+                CHECK(ref0 < ref3);
+                CHECK(ref0 <= ref3);
+                CHECK(ref0.value() < ref3);
+                CHECK(ref0.value() <= ref3);
+                CHECK(ref0 < ref3.value());
+                CHECK(ref0 <= ref3.value());
+
+                CHECK(ref3 > ref0);
+                CHECK(ref3 >= ref0);
+                CHECK(ref3.value() > ref0);
+                CHECK(ref3.value() >= ref0);
+                CHECK(ref3 > ref0.value());
+                CHECK(ref3 >= ref0.value());
+            }
+        }
+
+    }
+}
+
