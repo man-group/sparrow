910bfa16289d16e3c492f3d1d0aba2bfa7cd8707
Klaim (Joël Lamotte) <142265+Klaim@users.noreply.github.com>
2024-03-28 12:46:38 +0100

    refactor common native types traits

--- include/sparrow/data_traits.hpp
+++ include/sparrow/data_traits.hpp
@@ -9,6 +9,13 @@
 namespace sparrow
 {
 
+    template<class T>
+    struct common_native_types_traits
+    {
+        using value_type = T;
+        using default_layout = fixed_size_layout<T>;
+    };
+
     template<>
     struct arrow_traits<std::nullopt_t>
     {
@@ -19,111 +26,75 @@ namespace sparrow
     };
 
     template<>
-    struct arrow_traits<bool>
+    struct arrow_traits<bool> : common_native_types_traits<bool>
     {
         static constexpr data_type type_id = data_type::BOOL;
-        using value_type = bool;
-        using default_layout = fixed_size_layout<value_type>;
-
     };
 
     template<>
-    struct arrow_traits<std::uint8_t>
+    struct arrow_traits<std::uint8_t> : common_native_types_traits<std::uint8_t>
     {
         static constexpr data_type type_id = data_type::UINT8;
-        using value_type = std::uint8_t;
-        using default_layout = fixed_size_layout<value_type>;
-
     };
 
     template<>
-    struct arrow_traits<std::int8_t>
+    struct arrow_traits<std::int8_t> : common_native_types_traits<std::int8_t>
     {
         static constexpr data_type type_id = data_type::INT8;
-        using value_type = std::int8_t;
-        using default_layout = fixed_size_layout<value_type>;
-
     };
 
     template<>
-    struct arrow_traits<std::uint16_t>
+    struct arrow_traits<std::uint16_t> : common_native_types_traits<std::uint16_t>
     {
         static constexpr data_type type_id = data_type::UINT16;
-        using value_type = std::uint16_t;
-        using default_layout = fixed_size_layout<value_type>;
-
     };
 
     template<>
-    struct arrow_traits<std::int16_t>
+    struct arrow_traits<std::int16_t> : common_native_types_traits<std::int16_t>
     {
         static constexpr data_type type_id = data_type::INT16;
-        using value_type = std::int16_t;
-        using default_layout = fixed_size_layout<value_type>;
-
     };
 
     template<>
-    struct arrow_traits<std::uint32_t>
+    struct arrow_traits<std::uint32_t> : common_native_types_traits<std::uint32_t>
     {
         static constexpr data_type type_id = data_type::UINT32;
-        using value_type = std::uint32_t;
-        using default_layout = fixed_size_layout<value_type>;
-
     };
 
     template<>
-    struct arrow_traits<std::int32_t>
+    struct arrow_traits<std::int32_t> : common_native_types_traits<std::int32_t>
     {
         static constexpr data_type type_id = data_type::INT32;
-        using value_type = std::int32_t;
-        using default_layout = fixed_size_layout<value_type>;
-
     };
 
     template<>
-    struct arrow_traits<std::uint64_t>
+    struct arrow_traits<std::uint64_t> : common_native_types_traits<std::uint64_t>
     {
         static constexpr data_type type_id = data_type::UINT64;
-        using value_type = std::uint64_t;
-        using default_layout = fixed_size_layout<value_type>;
-
     };
 
     template<>
-    struct arrow_traits<std::int64_t>
+    struct arrow_traits<std::int64_t> : common_native_types_traits<std::int64_t>
     {
         static constexpr data_type type_id = data_type::INT64;
-        using value_type = std::int64_t;
-        using default_layout = fixed_size_layout<value_type>;
-
     };
 
     template<>
-    struct arrow_traits<float16_t>
+    struct arrow_traits<float16_t> : common_native_types_traits<float16_t>
     {
         static constexpr data_type type_id = data_type::HALF_FLOAT;
-        using value_type = float16_t;
-        using default_layout = fixed_size_layout<value_type>;
-
     };
 
     template<>
-    struct arrow_traits<float32_t>
+    struct arrow_traits<float32_t> : common_native_types_traits<float32_t>
     {
         static constexpr data_type type_id = data_type::FLOAT;
-        using value_type = float32_t;
-        using default_layout = fixed_size_layout<value_type>;
-
     };
 
     template<>
-    struct arrow_traits<float64_t>
+    struct arrow_traits<float64_t> : common_native_types_traits<float64_t>
     {
         static constexpr data_type type_id = data_type::DOUBLE;
-        using value_type = float64_t;
-        using default_layout = fixed_size_layout<value_type>;
-
     };
 
     template<>
