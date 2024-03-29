15a4fc44f33d6b5fc5fe19901aafacb24c819bad
Johan Mabille <johan.mabille@gmail.com>
2024-03-29 13:25:36 +0100

    Implemented pointer_iterator (#53)
    Implemented pointer_iterator, refactored buffer and fixed_size_layout iterators

--- include/sparrow/fixed_size_layout.hpp
+++ include/sparrow/fixed_size_layout.hpp
@@ -27,54 +27,6 @@
 namespace sparrow
 {
     /**
-     * An iterator for `fixed_size_layout` operating on contiguous data only.
-     *
-     * @tparam T The type of the elements in the layout's data buffer.
-     * @tparam is_const A boolean indicating whether the iterator is const.
-     *
-     */
-    template <class T, bool is_const>
-    class fixed_size_layout_value_iterator
-        : public iterator_base
-        <
-            fixed_size_layout_value_iterator<T, is_const>,
-            mpl::constify_t<T, is_const>,
-            std::contiguous_iterator_tag
-        >
-    {
-    public:
-
-        using self_type = fixed_size_layout_value_iterator<T, is_const>;
-        using base_type = iterator_base
-        <
-            self_type,
-            mpl::constify_t<T, is_const>,
-            std::contiguous_iterator_tag
-        >;
-        using pointer = typename base_type::pointer;
-
-        fixed_size_layout_value_iterator() = default;
-        explicit fixed_size_layout_value_iterator(pointer p);
-
-    private:
-        using reference = typename base_type::reference;
-        using difference_type = typename base_type::difference_type;
-        using size_type = std::size_t;
-
-        reference dereference() const;
-        void increment();
-        void decrement();
-        void advance(difference_type n);
-        difference_type distance_to(const self_type& hs) const;
-        bool equal(const self_type& rhs) const;
-        bool less_than(const self_type& rhs) const;
-
-        pointer m_pointer = nullptr;
-
-        friend class iterator_access;
-    };
-
-    /**
      * A contiguous layout for fixed size types.
      *
      * This class provides a contiguous layout for fixed size types, such as `uint8_t`, `int32_t`, etc.
@@ -106,10 +58,10 @@ namespace sparrow
         using difference_type = std::ptrdiff_t;
 
         using const_bitmap_iterator = array_data::bitmap_type::const_iterator;
-        using const_value_iterator = fixed_size_layout_value_iterator<T, true>;
+        using const_value_iterator = pointer_iterator<const_pointer>;
 
         using bitmap_iterator = array_data::bitmap_type::iterator;
-        using value_iterator = fixed_size_layout_value_iterator<T, false>;
+        using value_iterator = pointer_iterator<pointer>;
 
         using const_bitmap_range = std::ranges::subrange<const_bitmap_iterator>;
         using const_value_range = std::ranges::subrange<const_value_iterator>;
@@ -160,58 +112,6 @@ namespace sparrow
         friend class reference_proxy<fixed_size_layout>;
         friend class const_reference_proxy<fixed_size_layout>;
     };
-
-    /***************************************************
-     * fixed_size_layout_value_iterator implementation *
-     **************************************************/
-
-    template <class T, bool is_const>
-    fixed_size_layout_value_iterator<T, is_const>::fixed_size_layout_value_iterator(pointer pointer)
-        : m_pointer(pointer)
-    {
-    }
-
-    template <class T, bool is_const>
-    auto fixed_size_layout_value_iterator<T, is_const>::dereference() const -> reference
-    {
-        return *m_pointer;
-    }
-
-    template <class T, bool is_const>
-    void fixed_size_layout_value_iterator<T, is_const>::increment()
-    {
-        ++m_pointer;
-    }
-
-    template <class T, bool is_const>
-    void fixed_size_layout_value_iterator<T, is_const>::decrement()
-    {
-        --m_pointer;
-    }
-
-    template <class T, bool is_const>
-    void fixed_size_layout_value_iterator<T, is_const>::advance(difference_type n)
-    {
-        m_pointer += n;
-    }
-
-    template <class T, bool is_const>
-    auto fixed_size_layout_value_iterator<T, is_const>::distance_to(const self_type& rhs) const -> difference_type
-    {
-        return rhs.m_pointer - m_pointer;
-    }
-
-    template <class T, bool is_const>
-    bool fixed_size_layout_value_iterator<T, is_const>::equal(const self_type& rhs) const
-    {
-        return rhs.m_pointer == m_pointer;
-    }
-
-    template <class T, bool is_const>
-    bool fixed_size_layout_value_iterator<T, is_const>::less_than(const self_type& rhs) const
-    {
-        return m_pointer < rhs.m_pointer;
-    }
 
     /************************************
      * fixed_size_layout implementation *
