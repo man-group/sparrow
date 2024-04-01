b989598f3e06d7078bd50ebd2ca510c350cd98d4
Johan Mabille <johan.mabille@gmail.com>
2024-04-01 17:04:14 +0200

    Implemented type erasure for allocators

---
+++ include/sparrow/allocator.hpp
@@ -0,0 +1,148 @@
+// Copyright 2024 Man Group Operations Limited
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <cstdint>
+#include <memory>
+#include <type_traits>
+
+namespace sparrow
+{
+    /*
+     * allocator concept, based on the requirements specified by the standard:
+     * https://en.cppreference.com/w/cpp/named_req/Allocator.
+     */
+    template <class T>
+    concept allocator = std::copy_constructible<std::remove_cvref_t<T>>
+        and std::move_constructible<std::remove_cvref_t<T>>
+        and std::equality_comparable<std::remove_cvref_t<T>>
+        and requires(std::remove_cvref_t<T>& alloc,
+                     typename std::remove_cvref_t<T>::value_type* p,
+                     std::size_t n)
+        {
+            typename std::remove_cvref_t<T>::value_type;
+            { alloc.allocate(n) } -> std::same_as<typename std::remove_cvref_t<T>::value_type*>;
+            { alloc.deallocate(p, n) } -> std::same_as<void>;
+        };
+
+    /*
+     * Type erasure class for allocators. This allows to use any kind of allocator
+     * (standard, polymorphic) whithout having to expose it as a template parameter.
+     *
+     * @tparam T value_type of the allocator
+     * @tparam DA default allocator, instantiated when calling the default constructor
+     */
+    template <class T, class DA = std::allocator<T>>
+    class any_allocator
+    {
+    public:
+
+        using value_type = T;
+
+        any_allocator()
+            : p_allocator(std::make_unique<impl<DA>>(DA()))
+        {
+        }
+
+        any_allocator(const any_allocator& rhs)
+            : p_allocator(rhs.p_allocator->clone())
+        {
+        }
+
+        any_allocator& operator=(const any_allocator& rhs)
+        {
+            p_allocator = rhs.p_allocator->clone();
+            return *this;
+        }
+
+        any_allocator(any_allocator&&) = default;
+        any_allocator& operator=(any_allocator&&) = default;
+
+        template <class A>
+        any_allocator(A&& alloc)
+        requires (not std::same_as<std::remove_cvref_t<A>, any_allocator>
+                and sparrow::allocator<A>)
+            : p_allocator(std::make_unique<impl<std::decay_t<A>>>(std::forward<A>(alloc)))
+        {
+        }
+
+        [[nodiscard]] T* allocate(std::size_t n)
+        {
+            return p_allocator->allocate(n);
+        }
+
+        void deallocate(T* p, std::size_t n)
+        {
+            p_allocator->deallocate(p, n);
+        }
+
+        bool equal(const any_allocator& rhs) const
+        {
+            return p_allocator->equal(*rhs.p_allocator);
+        }
+
+    private:
+
+        struct interface
+        {
+            [[nodiscard]] virtual T* allocate(std::size_t) = 0;
+            virtual void deallocate(T*, std::size_t) = 0;
+            virtual std::unique_ptr<interface> clone() const = 0;
+            virtual bool equal(const interface&) const = 0;
+            virtual ~interface() = default;
+        };
+
+        template <allocator A>
+        struct impl : interface
+        {
+            A m_alloc;
+
+            explicit impl(A alloc) : m_alloc(std::move(alloc)) {}
+
+            [[nodiscard]] T* allocate(std::size_t n) override
+            {
+                return m_alloc.allocate(n);
+            }
+
+            void deallocate(T* p, std::size_t n) override
+            {
+                m_alloc.deallocate(p, n);
+            }
+
+            std::unique_ptr<interface> clone() const override
+            {
+                return std::make_unique<impl<A>>(m_alloc);
+            }
+
+            bool equal(const interface& rhs) const override
+            {
+                if (auto* p = dynamic_cast<const impl<A>*>(&rhs))
+                {
+                    return p->m_alloc == m_alloc;
+                }
+                return false;
+            }
+        };
+
+        std::unique_ptr<interface> p_allocator;
+    };
+
+    template <class T, class DA>
+    bool operator==(const any_allocator<T, DA>& lhs, const any_allocator<T, DA>& rhs)
+    {
+        return lhs.equal(rhs);
+    }
+}
+
