e6d52b95680795d92280474fb6c28619c1aebc81
Klaim (Joël Lamotte) <142265+Klaim@users.noreply.github.com>
2024-04-05 16:30:35 +0200

    fixed rebasing issue

--- include/sparrow/mp_utils.hpp
+++ include/sparrow/mp_utils.hpp
@@ -265,18 +265,5 @@ namespace sparrow::mpl
     }
 
 
-    namespace impl
-    {
-        template <class C, bool is_const>
-        struct get_inner_reference
-            : std::conditional<is_const, typename C::inner_const_reference, typename C::inner_reference>
-        {
-        };
 
-        template <class C, bool is_const>
-        using get_inner_reference_t = typename get_inner_reference<C, is_const>::type;
-    }  // namespace impl
-
-    template <class T>
-    concept layout_offset = std::same_as<T, std::int32_t> || std::same_as<T, std::int64_t>;
 }
