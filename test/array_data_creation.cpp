0721e0c968240bf4a9a368bd3450179b069a25b9
Julien Jerphanion <git@jjerphan.xyz>
2024-05-20 17:11:38 +0200

    wip: Adapt tests for sparrow::timestamp
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>

--- test/array_data_creation.cpp
+++ test/array_data_creation.cpp
@@ -1,29 +1,34 @@
 #include "array_data_creation.hpp"
 
-template <>
-sparrow::array_data
-make_test_array_data<sparrow::timestamp>(size_t n, size_t offset, const std::vector<size_t>& false_bitmap)
+namespace sparrow::test
 {
-    sparrow::array_data ad;
-    ad.type = sparrow::data_descriptor(sparrow::arrow_traits<sparrow::timestamp>::type_id);
-    ad.bitmap = sparrow::dynamic_bitset<uint8_t>(n, true);
-    for (const auto i : false_bitmap) {
-        if (i >= n) {
-            throw std::invalid_argument("Index out of range");
-        }
-        ad.bitmap.set(i, false);
-    }
-    const size_t buffer_size = (n * sizeof(sparrow::timestamp)) / sizeof(uint8_t);
-    sparrow::buffer<uint8_t> b(buffer_size);
+    using sys_time = std::chrono::time_point<std::chrono::system_clock, std::chrono::nanoseconds>;
 
-    for (uint8_t i = 0; i < n; ++i)
+    template <>
+    sparrow::array_data
+    make_test_array_data<sparrow::timestamp>(size_t n, size_t offset, const std::vector<size_t>& false_bitmap)
     {
-        b.data<sparrow::timestamp>()[i] = sparrow::timestamp(i);
-    }
+        sparrow::array_data ad;
+        ad.type = sparrow::data_descriptor(sparrow::arrow_traits<sparrow::timestamp>::type_id);
+        ad.bitmap = sparrow::dynamic_bitset<uint8_t>(n, true);
+        for (const auto i : false_bitmap) {
+            if (i >= n) {
+                throw std::invalid_argument("Index out of range");
+            }
+            ad.bitmap.set(i, false);
+        }
+        const size_t buffer_size = (n * sizeof(sparrow::timestamp)) / sizeof(uint8_t);
+        sparrow::buffer<uint8_t> b(buffer_size);
 
-    ad.buffers.push_back(b);
-    ad.length = n;
-    ad.offset = offset;
-    ad.child_data.emplace_back();
-    return ad;
-}
+        for (uint8_t i = 0; i < n; ++i)
+        {
+            b.data<sparrow::timestamp>()[i] = sparrow::timestamp(sys_time(std::chrono::system_clock::now()));
+        }
+
+        ad.buffers.push_back(b);
+        ad.length = n;
+        ad.offset = offset;
+        ad.child_data.emplace_back();
+        return ad;
+    }
+}  // namespace sparrow::test
