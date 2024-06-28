044f59c070bd0d0c195971528c4d6f9a5057ecfb
DerThorsten <derthorstenbeier@gmail.com>
2024-06-28 10:28:52 +0200

    add doxygen documentation + examples

---
+++ examples/hello_world.cpp
@@ -0,0 +1,11 @@
+/** @example hello_world.cpp
+ * A description of the example file, causes the example file to show up in 
+ * Examples */
+
+#include <sparrow/array_data.hpp>
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
