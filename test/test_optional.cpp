87a4055ab1b28d14091ee31ad39602bd74432685
Johan Mabille <johan.mabille@gmail.com>
2024-07-05 09:57:45 +0200

    Update test/test_optional.cpp
    Co-authored-by: Alexis Placet <alexis.placet@gmail.com>

--- test/test_optional.cpp
+++ test/test_optional.cpp
@@ -164,7 +164,7 @@ namespace sparrow
                 optional_int d1(d0);
                 optional_double d2(2.5);
                 d2 = std::move(d0);
-                CHECK(d2.has_value());
+                REQUIRE(d2.has_value());
                 CHECK_EQ(d1.value(), d2.value());
             }
             {
