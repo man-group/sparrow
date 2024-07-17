1872298d40e2078882f45e2496b84f43d084b1b2
Alexis Placet <alexis.placet@quantstack.net>
2024-07-17 14:23:51 +0200

    Add UT

--- test/test_arrow_array_schema_utils.cpp
+++ test/test_arrow_array_schema_utils.cpp
@@ -46,5 +46,116 @@ TEST_SUITE("C Data Interface")
                 CHECK_EQ(size, 3);
             }
         }
+
+        SUBCASE("get_raw_ptr")
+        {
+            SUBCASE("int")
+            {
+                int i = 5;
+                auto raw_ptr = sparrow::get_raw_ptr<int>(i);
+                CHECK_EQ(*raw_ptr, 5);
+            }
+
+            SUBCASE("int*")
+            {
+                int i = 5;
+                int* ptr = &i;
+                auto raw_ptr = sparrow::get_raw_ptr<int>(ptr);
+                CHECK_EQ(*raw_ptr, 5);
+            }
+
+            SUBCASE("std::unique_ptr<int>")
+            {
+                std::unique_ptr<int> ptr = std::make_unique<int>(5);
+                auto raw_ptr = sparrow::get_raw_ptr<int>(ptr);
+                CHECK_EQ(*raw_ptr, 5);
+            }
+
+            SUBCASE("std::unique_ptr<int[]>")
+            {
+                std::unique_ptr<int[]> ptr = std::make_unique<int[]>(5);
+                auto raw_ptr = sparrow::get_raw_ptr<int>(ptr);
+                CHECK_EQ(raw_ptr[0], 0);
+            }
+
+            SUBCASE("std::vector<int>")
+            {
+                std::vector<int> vec = {0, 1, 2, 3, 4, 5};
+                auto raw_ptr = sparrow::get_raw_ptr<int>(vec);
+                for (size_t i = 0; i < vec.size(); ++i)
+                {
+                    CHECK_EQ(raw_ptr[i], vec[i]);
+                }
+            }
+
+            SUBCASE("std::unique_ptr<sparrow::buffer<int>>")
+            {
+                std::unique_ptr<sparrow::buffer<int>> ptr = std::make_unique<sparrow::buffer<int>>(5);
+                for (size_t i = 0; i < ptr->size(); ++i)
+                {
+                    ptr->data()[i] = 0;
+                }
+                auto raw_ptr = sparrow::get_raw_ptr<int>(ptr);
+                for (size_t i = 0; i < ptr->size(); ++i)
+                {
+                    CHECK_EQ(raw_ptr[i], 0);
+                }
+            }
+        }
+
+        SUBCASE("to_raw_ptr_vec")
+        {
+            SUBCASE("std::vector<std::unique_ptr<int>>")
+            {
+                std::vector<std::unique_ptr<int>> vec;
+                for (int i = 0; i < 5; ++i)
+                {
+                    vec.push_back(std::make_unique<int>(i));
+                }
+                auto raw_ptr_vec = sparrow::to_raw_ptr_vec<int>(vec);
+                for (size_t i = 0; i < vec.size(); ++i)
+                {
+                    CHECK_EQ(*raw_ptr_vec[i], i);
+                }
+            }
+
+            SUBCASE("std::vector<int>")
+            {
+                std::vector<int> vec = {0, 1, 2, 3, 4, 5};
+                auto raw_ptr_vec = sparrow::to_raw_ptr_vec<int>(vec);
+                for (size_t i = 0; i < vec.size(); ++i)
+                {
+                    CHECK_EQ(*raw_ptr_vec[i], vec[i]);
+                }
+            }
+
+            SUBCASE("std::vector<std::shared_ptr<int>>")
+            {
+                std::vector<std::shared_ptr<int>> vec;
+                for (int i = 0; i < 5; ++i)
+                {
+                    vec.push_back(std::make_shared<int>(i));
+                }
+                auto raw_ptr_vec = sparrow::to_raw_ptr_vec<int>(vec);
+                for (size_t i = 0; i < vec.size(); ++i)
+                {
+                    CHECK_EQ(*raw_ptr_vec[i], i);
+                }
+            }
+
+            SUBCASE("std::vector<int*>")
+            {
+                std::vector<int*> vec;
+                for (int i = 0; i < 5; ++i)
+                {
+                    vec.emplace_back(new int(i));
+                }
+                auto raw_ptr_vec = sparrow::to_raw_ptr_vec<int>(vec);
+                for (size_t i = 0; i < vec.size(); ++i)
+                {
+                    CHECK_EQ(*raw_ptr_vec[i], i);
+                }
+            }
+        }
     }
 }
