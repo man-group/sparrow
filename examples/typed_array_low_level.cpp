86d8c1eb8e36449a88756088d4f58858105b2184
Klaim (Joël Lamotte) <142265+Klaim@users.noreply.github.com>
2024-07-31 18:36:41 +0200

    Re-activated typed_array_low_level example

--- examples/typed_array_low_level.cpp
+++ examples/typed_array_low_level.cpp
@@ -12,37 +12,38 @@ int main() {
     // create array_data with 5 float32 elements
     // where the value at index 2 is missing
     /////////////////////////////////////////////////
-    /*using value_type = float;
+    using value_type = float;
+    static constexpr float no_value = NAN;
     auto n = 5;
 
-    // create the array_data object 
-    sparrow::array_data ad;
-    ad.type = sparrow::data_descriptor(sparrow::arrow_traits<value_type>::type_id);
+    // create the array_data object
+    sparrow::array_data data;
+    data.type = sparrow::data_descriptor(sparrow::arrow_traits<value_type>::type_id);
 
-    // create a bitmap with all bits set to true exceot for 
+    // create a bitmap with all bits set to true except for
     // the missing value at index 2
-    ad.bitmap = sparrow::dynamic_bitset<uint8_t>(n, true);
-    ad.bitmap.set(2, false);
+    data.bitmap = sparrow::dynamic_bitset<uint8_t>(n, true);
+    data.bitmap.set(2, false);
 
 
     //  the buffer holding the actual data
     const size_t buffer_size = (n * sizeof(value_type)) / sizeof(uint8_t);
-    sparrow::buffer<uint8_t> b(buffer_size);
+    sparrow::buffer<uint8_t> buffer(buffer_size);
 
-    b.data<value_type>()[0] = 1.0;
-    b.data<value_type>()[1] = 2.0;
-    // b.data<value_type>()[2] = ???; // missing value
-    b.data<value_type>()[3] = 4.0;
-    b.data<value_type>()[4] = 5.0;
+    buffer.data<value_type>()[0] = 1.0;
+    buffer.data<value_type>()[1] = 2.0;
+    buffer.data<value_type>()[2] = no_value;  // missing value, will lead to errors if used
+    buffer.data<value_type>()[3] = 4.0;
+    buffer.data<value_type>()[4] = 5.0;
 
     // add the buffer to the array_data
-    ad.buffers.push_back(b);
-    ad.length = static_cast<std::int64_t>(n);
-    ad.offset = static_cast<std::int64_t>(0);
-    ad.child_data.emplace_back();
-    
+    data.buffers.push_back(buffer);
+    data.length = static_cast<std::int64_t>(n);
+    data.offset = static_cast<std::int64_t>(0);
+    data.child_data.emplace_back();
+
     // create a typed_array object from the array_data
-    auto array = sparrow::typed_array<value_type>(ad);
+    auto array = sparrow::typed_array<value_type>(data);
 
     // access the data
     for (auto i = 0; i < n; ++i) {
@@ -51,7 +52,7 @@ int main() {
         } else {
             std::cout << "missing value" << std::endl;
         }
-    }*/
+    }
 
     return 0;
 }
