a9e213ebf5fb491719498220c445cfb32a166a72
Klaim (Joël Lamotte) <142265+Klaim@users.noreply.github.com>
2024-08-06 15:56:28 +0200

    Added some tests

--- test/test_traits.cpp
+++ test/test_traits.cpp
@@ -14,6 +14,7 @@
 
 #include <sparrow/array/data_traits.hpp>
 
+
 /////////////////////////////////////////////////////////////////////////////////////////
 // Opt-in support for custom C++ representations of arrow data types.
 
@@ -39,4 +40,117 @@ namespace sparrow
 {
     static_assert(mpl::all_of(all_base_types_t{}, predicate::is_arrow_base_type));
     static_assert(mpl::all_of(all_base_types_t{}, predicate::has_arrow_traits));
+
+
+// Native basic standard types support
+
+    using basic_native_types = mpl::typelist<
+        bool,
+        char, unsigned char, signed char,
+        short, unsigned short,
+        int, unsigned int,
+        long, unsigned long, // `long long` could be bigger than 64bits and is not supported
+        float, double, // `long double` could be bigger than 64bit and is not supported
+        std::uint8_t,
+        std::int8_t,
+        std::uint16_t,
+        std::int16_t,
+        std::uint32_t,
+        std::int32_t,
+        std::uint64_t,
+        std::int64_t,
+        float16_t,
+        float32_t,
+        float64_t
+        >;
+
+    template <std::integral T>
+    consteval
+    bool is_possible_arrow_data_type(data_type type_id)
+    {
+        // NOTE:
+        // `char` is not specified by the C and C++ standard to be `signed` or `unsigned`.
+        // The language also specifies `signed char` and `unsigned char` as distinct types from `char`.
+        // Therefore, for `char`, sign-ness can vary depending on the C++ target platform and compiler.
+        // The best we can check is that `sizeof(char)` matches the associated arrow data
+        // integral type minimum size, whatever the sign-ness, which is why we don't treat
+        // `char` as a special case below.
+
+        if constexpr (std::same_as<T, bool>)
+        {
+            return type_id == data_type::BOOL;
+        }
+        // we need T to be able to store the data coming from an arrow data value
+        // of the associated arrow data type
+        else if constexpr (std::is_signed_v<T>)
+        {
+            switch (type_id)
+            {
+                case data_type::INT8:
+                    return sizeof(T) == 1;
+                case data_type::INT16:
+                    return sizeof(T) <= 2;
+                case data_type::INT32:
+                    return sizeof(T) <= 4;
+                case data_type::INT64:
+                    return sizeof(T) <= 8;
+            }
+        }
+        else
+        {
+            static_assert(std::is_unsigned_v<T>);
+
+            switch (type_id)
+            {
+                case data_type::UINT8:
+                    return sizeof(T) == 1;
+                case data_type::UINT16:
+                    return sizeof(T) <= 2;
+                case data_type::UINT32:
+                    return sizeof(T) <= 4;
+                case data_type::UINT64:
+                    return sizeof(T) <= 8;
+            }
+        }
+
+        return false;
+    }
+
+    template <std::floating_point T>
+    consteval
+    bool is_possible_arrow_data_type(data_type type_id)
+    {
+        switch (type_id)
+        {
+            case data_type::HALF_FLOAT:
+                return sizeof(T) <= 2;
+            case data_type::FLOAT:
+                return sizeof(T) <= 4;
+            case data_type::DOUBLE:
+                return sizeof(T) <= 8;
+        }
+
+        return false;
+    }
+
+    // Tests `data_type_from_size` and it's usage in `arrow_traits<T>::type_id`
+    struct
+    {
+        template <class T>
+            requires has_arrow_type_traits<T>
+        consteval
+        bool operator()(mpl::typelist<T>)
+        {
+            constexpr auto deduced_type_id = data_type_from_size<T>();
+            static_assert(deduced_type_id == arrow_traits<T>::type_id);
+
+            return is_possible_arrow_data_type<T>(arrow_traits<T>::type_id);
+        }
+    } constexpr has_possible_arrow_data_type;
+
+    // Every basic native types must have an arrow trait, whatever the platform,
+    // including when fixed-size standard library names are or not alias to basic types.
+    // Only exceptions: types that could be bigger than 64bit (`long long`, `long double`, etc.)
+    static_assert(mpl::all_of(basic_native_types{}, has_possible_arrow_data_type));
+
 }
