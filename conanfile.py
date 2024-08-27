e438e9bcd7d0a728a18b0d586381c55f65d2eed9
Alexis Placet <alexis.placet@quantstack.net>
2024-08-27 09:13:59 +0200

    Update

--- conanfile.py
+++ conanfile.py
@@ -11,7 +11,7 @@ class SparrowRecipe(ConanFile):
     name = "sparrow"
     description = "C++20 idiomatic APIs for the Apache Arrow Columnar Format"
     license = "Apache-2.0"
-    author = "ManGroup"
+    author = "Man Group"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/man-group/sparrow"
     topics = ("arrow", "header-only")
