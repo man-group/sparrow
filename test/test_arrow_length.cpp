2c559d581ea5dd2c9a539d369c8f4c39b3e8a437
Klaim (Joël Lamotte) <142265+Klaim@users.noreply.github.com>
2024-10-02 15:00:10 +0200

    tests: is_valid_arrow_length

---
+++ test/test_arrow_length.cpp
@@ -0,0 +1,72 @@
+// Copyright 2024 Man Group Operations Limited
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <cstddef>
+#include <vector>
+
+#include "doctest/doctest.h"
+
+#include "sparrow/array/data_type.hpp"
+
+
+
+TEST_SUITE("value_ptr")
+{
+    TEST_CASE("is_valid_arrow_length")
+    {
+        using namespace sparrow;
+        CHECK(is_valid_arrow_length(0));
+        CHECK(is_valid_arrow_length(1));
+        CHECK(is_valid_arrow_length(1024));
+        CHECK(is_valid_arrow_length(std::numeric_limits<std::int32_t>::max()));
+        CHECK(is_valid_arrow_length(max_arrow_length));
+
+        CHECK_FALSE(is_valid_arrow_length(-1));
+        CHECK_FALSE(is_valid_arrow_length(-1024));
+        CHECK_FALSE(is_valid_arrow_length(std::numeric_limits<std::int32_t>::min()));
+        CHECK(is_valid_arrow_length(-1, arrow_length_kind::offset));
+        CHECK(is_valid_arrow_length(-1024, arrow_length_kind::offset));
+        CHECK(is_valid_arrow_length(std::numeric_limits<std::int32_t>::min(), arrow_length_kind::offset));
+
+        CHECK(is_valid_arrow_length(std::size_t{0}));
+        CHECK(is_valid_arrow_length(std::size_t{1}));
+        CHECK(is_valid_arrow_length(std::ptrdiff_t{0}));
+        CHECK(is_valid_arrow_length(std::ptrdiff_t{1}));
+        CHECK(is_valid_arrow_length(std::ptrdiff_t{-1}, arrow_length_kind::offset));
+
+        // When not contrained to 32bit length and native offsets can represent less or equal values of arrow lengh,
+        // we check that the native offset types are usable as expected as arrow length.
+        if constexpr( sizeof(std::ptrdiff_t) <= max_arrow_length and config::enable_32bit_size_limit == false )
+        {
+            CHECK(is_valid_arrow_length(std::numeric_limits<std::ptrdiff_t>::max()));
+            CHECK(is_valid_arrow_length(std::numeric_limits<std::ptrdiff_t>::max(), arrow_length_kind::offset));
+            CHECK(is_valid_arrow_length(std::numeric_limits<std::ptrdiff_t>::min(), arrow_length_kind::offset));
+        }
+
+        // We always support at least 32bit lengths
+        CHECK(is_valid_arrow_length(std::size_t(std::numeric_limits<std::int32_t>::max())));
+        CHECK(is_valid_arrow_length(std::ptrdiff_t(std::numeric_limits<std::int32_t>::max())));
+        CHECK(is_valid_arrow_length(
+            std::ptrdiff_t(std::numeric_limits<std::int32_t>::max()),
+            arrow_length_kind::offset
+        ));
+        CHECK(is_valid_arrow_length(
+            std::ptrdiff_t(std::numeric_limits<std::int32_t>::min()),
+            arrow_length_kind::offset
+        ));
+    }
+
+
+
+}
