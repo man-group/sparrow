2baa1ed03f9cc8a0018e9e0f06307ad6ea4bda46
Alexis Placet <alexis.placet@quantstack.net>
2024-11-14 09:05:46 +0100

    Add record_batch example, improve documentation, add asserts (#285)
    Add record_batch example, improve documentation, add asserts

---
+++ examples/record_batch_example.cpp
@@ -0,0 +1,52 @@
+/**
+ * Example of usage of the record_batch class.
+ */
+
+#include <cassert>
+
+#include "sparrow/layout/primitive_array.hpp"
+#include "sparrow/record_batch.hpp"
+
+std::vector<sparrow::array> make_array_list(const std::size_t data_size)
+{
+    sparrow::primitive_array<std::uint16_t> pr0(
+        std::ranges::iota_view{std::size_t(0), std::size_t(data_size)}
+        | std::views::transform(
+            [](auto i)
+            {
+                return static_cast<std::uint16_t>(i);
+            }
+        )
+    );
+    sparrow::primitive_array<std::int32_t> pr1(
+        std::ranges::iota_view{std::int32_t(4), 4 + std::int32_t(data_size)}
+    );
+    sparrow::primitive_array<std::int32_t> pr2(
+        std::ranges::iota_view{std::int32_t(2), 2 + std::int32_t(data_size)}
+    );
+
+    std::vector<sparrow::array> arr_list = {
+        sparrow::array{std::move(pr0)},
+        sparrow::array{std::move(pr1)},
+        sparrow::array{std::move(pr2)}
+    };
+    return arr_list;
+}
+
+int main()
+{
+    //! [use_record_batch]
+    const std::vector<std::string> name_list = {"first", "second", "third"};
+    constexpr std::size_t data_size = 10;
+    const std::vector<sparrow::array> array_list = make_array_list(data_size);
+    const sparrow::record_batch record{name_list, array_list};
+    assert(record.nb_columns() == array_list.size());
+    assert(record.nb_rows() == data_size);
+    assert(record.contains_column(name_list[0]));
+    assert(record.get_column_name(0) == name_list[0]);
+    assert(record.get_column(0) == array_list[0]);
+    assert(std::ranges::equal(record.names(), name_list));
+    assert(std::ranges::equal(record.columns(), array_list));
+    //! [use_record_batch]
+    return EXIT_SUCCESS;
+}
