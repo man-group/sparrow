da77f2939bbcfa08a29f8002cd184ccf6f65e21b
DerThorsten <derthorstenbeier@gmail.com>
2024-11-27 12:08:52 +0100

    more cleanup

--- include/sparrow/arrow_interface/arrow_array_schema_info_utils.hpp
+++ include/sparrow/arrow_interface/arrow_array_schema_info_utils.hpp
@@ -24,38 +24,6 @@
 
 namespace sparrow
 {
-    enum class buffer_type : uint8_t
-    {
-        VALIDITY,
-        DATA,
-        OFFSETS_32BIT,
-        OFFSETS_64BIT,
-        VIEWS,
-        TYPE_IDS,
-        SIZES_32BIT,
-        SIZES_64BIT,
-    };
-
-    /// @returns The expected offset element count for a given data type and array length.
-    constexpr std::size_t get_offset_element_count(data_type data_type, size_t length, size_t offset)
-    {
-        switch (data_type)
-        {
-            case data_type::STRING:
-            case data_type::LIST:
-            case data_type::LARGE_LIST:
-                return length + offset + 1;
-            case data_type::LIST_VIEW:
-            case data_type::LARGE_LIST_VIEW:
-            case data_type::DENSE_UNION:
-                return length + offset;
-            default:
-                throw std::runtime_error("Unsupported data type");
-        }
-    }
-
-   
-
     constexpr bool has_bitmap(data_type dt)
     {
         switch (dt)
@@ -101,13 +69,6 @@ namespace sparrow
     inline bool validate_format_with_arrow_array(data_type, const ArrowArray&)
     {
         return true;
-        /* THE CODE BELOW MAKES WRONG ASSUMPTIONS AND NEEDS TO BE REFACTORED IN A SEPERATE PR*/
-        // const bool buffers_count_valid = validate_buffers_count(data_type, array.n_buffers);
-        // // const bool children_count_valid = static_cast<std::size_t>(array.n_children)
-        // //                                   == get_expected_children_count(data_type);
-
-        // //std::cout<<"child cound: "<<array.n_children<<" expected:
-        // "<<get_expected_children_count(data_type)<<std::endl; return buffers_count_valid //&&
-        // children_count_valid;
+        /* THE CODE USED TO MAKES WRONG ASSUMPTIONS AND NEEDS TO BE REFACTORED IN A SEPERATE PR*/
     }
 }
