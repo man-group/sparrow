ed623137ebe1718cf42b366ac2f8c66d7e66b9fb
Alexis Placet <alexis.placet@quantstack.net>
2024-12-09 15:33:29 +0100

    Add Windows CI

--- cmake/code_coverage.cmake
+++ cmake/code_coverage.cmake
@@ -1,14 +1,10 @@
-# if(NOT CMAKE_CXX_COMPILER_ID MATCHES "Clang" AND NOT CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-#     message(WARNING "Code coverage is only supported with Clang or GCC")
-#     return()
-# endif()
-
 if(NOT CMAKE_BUILD_TYPE STREQUAL "Debug")
     message(WARNING "Code coverage results with an optimised (non-Debug) build may be misleading")
 endif()
 
 set(COVERAGE_REPORT_PATH "${CMAKE_BINARY_DIR}/coverage_reports" CACHE PATH "Path to store coverage reports")
 set(COBERTURA_REPORT_PATH "${COVERAGE_REPORT_PATH}/cobertura.xml" CACHE PATH "Path to store cobertura report")
+set(COVERAGE_TARGETS_FOLDER "Tests utilities/Code Coverage")
 
 if(CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
     find_program(OpenCPPCoverage OpenCppCoverage REQUIRED)
@@ -24,7 +20,7 @@ if(CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
         DEPENDS test_sparrow_lib
         COMMENT "Generating coverage cobertura report with OpenCppCoverage: ${COBERTURA_REPORT_PATH}"
     )
-    set(TARGET_PROPERTIES generate_cobertura PROPERTIES FOLDER "DQSDQSD")
+    set(TARGET_PROPERTIES generate_cobertura PROPERTIES FOLDER ${COVERAGE_TARGETS_FOLDER})
 
     add_custom_target(generate_html_coverage_report
         COMMAND ${OpenCPPCoverage}
@@ -34,7 +30,8 @@ if(CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
         DEPENDS test_sparrow_lib
         COMMENT "Generating coverage report with OpenCppCoverage: ${COVERAGE_REPORT_PATH}"
     )
-elseif(NOT CMAKE_CXX_COMPILER_ID MATCHES "Clang" AND NOT CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    set(TARGET_PROPERTIES generate_cobertura PROPERTIES FOLDER "Tests utilities/Code Coverage")
+elseif(CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
     find_program(llvm-cov llvm-cov REQUIRED)
     find_program(gcovr gcovr REQUIRED)
     find_program(doxide doxide REQUIRED PATHS ${doxide_ROOT})       
@@ -49,16 +46,14 @@ elseif(NOT CMAKE_CXX_COMPILER_ID MATCHES "Clang" AND NOT CMAKE_CXX_COMPILER_ID S
         add_custom_target(merge_gcov
             COMMAND ${CMAKE_COMMAND} -E rm -f "${CMAKE_BINARY_DIR}/coverage.gcov"
             COMMAND for /r %f in (*.gcda) do ${gcov_executable} --stdout "%f" >> ${CMAKE_BINARY_DIR}/coverage.gcov
-            DEPENDS run_tests
-        )
+            DEPENDS run_tests)
     else()
         add_custom_target(merge_gcov
             COMMAND ${CMAKE_COMMAND} -E rm -f "${CMAKE_BINARY_DIR}/coverage.gcov"
             COMMAND find . -name '*.gcda' | xargs ${gcov_executable} --stdout > ${CMAKE_BINARY_DIR}/coverage.gcov
-            DEPENDS run_tests
-        )
+            DEPENDS run_tests)
     endif()
-    set_target_properties(merge_gcov PROPERTIES FOLDER "Code Coverage")
+    set_target_properties(merge_gcov PROPERTIES FOLDER ${COVERAGE_TARGETS_FOLDER})
 
     add_custom_target(doxide_generate_json
         COMMAND ${doxide} cover --coverage ${CMAKE_BINARY_DIR}/coverage.gcov > ${CMAKE_BINARY_DIR}/doxide_coverage.json
@@ -66,7 +61,7 @@ elseif(NOT CMAKE_CXX_COMPILER_ID MATCHES "Clang" AND NOT CMAKE_CXX_COMPILER_ID S
         WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
         COMMENT "Generating coverage report with doxide: ${CMAKE_BINARY_DIR}/doxide_coverage.json"
     )
-    set_target_properties(doxide_generate_json PROPERTIES FOLDER "Code Coverage")
+    set_target_properties(doxide_generate_json PROPERTIES FOLDER ${COVERAGE_TARGETS_FOLDER})
 
     add_custom_target(generate_html_coverage_report
         COMMAND ${gcovr} --html-details ${COVERAGE_REPORT_PATH}/coverage.html
@@ -75,9 +70,8 @@ elseif(NOT CMAKE_CXX_COMPILER_ID MATCHES "Clang" AND NOT CMAKE_CXX_COMPILER_ID S
             --json-add-tracefile doxide_coverage.json
             -r ${CMAKE_SOURCE_DIR}
         DEPENDS doxide_generate_json
-        COMMENT "Generating coverage report with gcovr: ${COVERAGE_REPORT_PATH}/coverage.html"
-    )
-    set_target_properties(generate_html_with_gcovr PROPERTIES FOLDER "Code Coverage")
+        COMMENT "Generating coverage report with gcovr: ${COVERAGE_REPORT_PATH}/coverage.html")
+    set_target_properties(generate_html_coverage_report PROPERTIES FOLDER ${COVERAGE_TARGETS_FOLDER})
 
     add_custom_target(generate_cobertura
         COMMAND ${gcovr} --xml -o ${COBERTURA_REPORT_PATH}
@@ -86,9 +80,8 @@ elseif(NOT CMAKE_CXX_COMPILER_ID MATCHES "Clang" AND NOT CMAKE_CXX_COMPILER_ID S
             --json-add-tracefile doxide_coverage.json
             -r ${CMAKE_SOURCE_DIR}
         DEPENDS doxide_generate_json
-        COMMENT "Generating coverage report with gcovr: ${CMAKE_BINARY_DIR}/cobertura.xml"
-    )
-    set_target_properties(generate_cobertura PROPERTIES FOLDER "Test utilities/Code Coverage")
+        COMMENT "Generating coverage report with gcovr: ${CMAKE_BINARY_DIR}/cobertura.xml")
+    set_target_properties(generate_cobertura PROPERTIES FOLDER ${COVERAGE_TARGETS_FOLDER})
 endif()
 
 function(enable_coverage target)
@@ -97,10 +90,8 @@ function(enable_coverage target)
 
     target_compile_options(${target} PRIVATE
         $<$<CXX_COMPILER_ID:Clang>:${CLANG_COVERAGE_FLAGS}>
-        $<$<CXX_COMPILER_ID:GNU>:${GCC_COVERAGE_FLAGS}>
-    )
+        $<$<CXX_COMPILER_ID:GNU>:${GCC_COVERAGE_FLAGS}>)
     target_link_options(${target} PRIVATE
         $<$<CXX_COMPILER_ID:Clang>:${CLANG_COVERAGE_FLAGS}>
-        $<$<CXX_COMPILER_ID:GNU>:${GCC_COVERAGE_FLAGS}>
-    )
+        $<$<CXX_COMPILER_ID:GNU>:${GCC_COVERAGE_FLAGS}>)
 endfunction(enable_coverage target)
