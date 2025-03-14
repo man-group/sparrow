7ae32ae1aeb73aadc8bd6e638ff07e383f1124f5
Alexis Placet <alexis.placet@quantstack.net>
2025-03-14 13:31:04 +0100

    wip

--- test/integration/c_data_integration.cpp
+++ test/integration/c_data_integration.cpp
@@ -476,18 +476,17 @@ sparrow::array floating_point_from_json(const nlohmann::json& array, const nlohm
 sparrow::array decimal_from_json(const nlohmann::json& array, const nlohmann::json& schema)
 {
     check_type(array, schema, "decimal");
-    throw std::runtime_error("Not implemented");
+    const std::string name = schema.at("name").get<std::string>();
     const uint32_t precision = schema.at("type").at("precision").get<uint32_t>();
     const uint32_t scale = schema.at("type").at("scale").get<uint32_t>();
-    const uint32_t byte_width = schema.at("type").at("byteWidth").get<uint32_t>();
-    const std::string name = schema.at("name").get<std::string>();
+    const uint32_t byte_width = schema.at("type").at("bitWidth").get<uint32_t>();
     auto data_str = array.at(DATA).get<std::vector<std::string>>();
 
     if (byte_width == 32)
     {
         auto data = data_str
                     | std::views::transform(
-                        [](const std::string& value)
+                        [](const std::string& value) -> int32_t
                         {
                             return std::stoi(value);
                         }
@@ -495,7 +494,50 @@ sparrow::array decimal_from_json(const nlohmann::json& array, const nlohmann::js
         return sparrow::array{
             sparrow::decimal_32_array{data, get_validity(array), precision, scale, name, get_metadata(schema)}
         };
-    };
+    }
+    else if (byte_width == 64)
+    {
+        auto data = data_str
+                    | std::views::transform(
+                        [](const std::string& value) -> int64_t
+                        {
+                            return std::stoll(value);
+                        }
+                    );
+        return sparrow::array{
+            sparrow::decimal_64_array{data, get_validity(array), precision, scale, name, get_metadata(schema)}
+        };
+    }
+    else if (byte_width == 128)
+    {
+        auto data = data_str
+                    | std::views::transform(
+                        [](const std::string& value_str) -> sparrow::int128_t
+                        {
+                            return sparrow::stobigint<sparrow::int128_t>(value_str);
+                        }
+                    );
+        return sparrow::array{
+            sparrow::decimal_128_array{data, get_validity(array), precision, scale, name, get_metadata(schema)}
+        };
+    }
+    else if (byte_width == 256)
+    {
+        auto data = data_str
+                    | std::views::transform(
+                        [](const std::string& value_str)
+                        {
+                            return sparrow::stobigint<sparrow::int256_t>(value_str);
+                        }
+                    );
+        return sparrow::array{
+            sparrow::decimal_256_array{data, get_validity(array), precision, scale, name, get_metadata(schema)}
+        };
+    }
+    else
+    {
+        throw std::runtime_error("Invalid byte width for decimal type");
+    }
 }
 
 sparrow::array fixedsizebinary_from_json(const nlohmann::json& array, const nlohmann::json& schema)
