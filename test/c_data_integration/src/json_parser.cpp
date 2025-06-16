356692c25640333a99568a629c9596649d34d79f
Alexis Placet <alexis.placet@quantstack.net>
2025-06-16 10:56:14 +0200

    wip

--- test/c_data_integration/src/json_parser.cpp
+++ test/c_data_integration/src/json_parser.cpp
@@ -35,7 +35,6 @@
 
 namespace sparrow::c_data_integration
 {
-
     // Unordered map witk key = type name and value = function
     using array_builder_function = std::function<
         sparrow::array(const nlohmann::json&, const nlohmann::json&, const nlohmann::json&)>;
@@ -43,6 +42,7 @@ namespace sparrow::c_data_integration
         {"binary", binary_array_from_json},
         {"bool", bool_array_from_json},
         {"decimal", decimal_from_json},
+        {"dictionary", dictionary_encode_array_from_json}
         {"fixedsizebinary", fixedsizebinary_from_json},
         {"fixedsizelist", fixed_size_list_array_from_json},
         {"floatingpoint", floating_point_from_json},
@@ -166,39 +166,6 @@ namespace sparrow::c_data_integration
             }
         }
         throw std::runtime_error("Invalid bit width or signedness");
-    }
-
-    void read_schema_from_json(const nlohmann::json& data)
-    {
-        SPARROW_ASSERT_TRUE(data.is_object());
-        const auto fields_it = data.find("fields");
-        if (fields_it != data.end())
-        {
-            SPARROW_ASSERT_TRUE(fields_it->is_array());
-            for (const auto& field : *fields_it)
-            {
-                SPARROW_ASSERT_TRUE(field.is_object());
-
-                const std::string name = field.at("name").get<std::string>();
-                [[maybe_unused]] const bool nullable = field.at("nullable").get<bool>();
-                const auto type = field.at("type");
-
-                // TODO: support dictionary
-                // const auto dictionary_it = field.find("dictionary");
-                // if (dictionary_it != field.end())
-                // {
-                //     SPARROW_ASSERT_TRUE(dictionary_it->is_object());
-                //     const auto id_it = field.find("type");
-                // }
-
-                const auto children_it = field.find("children");
-                if (children_it != field.end())
-                {
-                    SPARROW_ASSERT_TRUE(children_it->is_array());
-                    read_schema_from_json(*children_it);
-                }
-            }
-        }
     }
 
     sparrow::array build_array_from_json(
