98b0b7caddd03a3efc1da7e90b813d61d317bce9
GitHub Action <runner@pkrvmccyg1gnepe.sj4etpwor4vuhlgevtwkpoierg.phxx.internal.cloudapp.net>
2025-08-29 12:53:26 +0000

    Release version 1.1.0

--- include/sparrow/config/sparrow_version.hpp
+++ include/sparrow/config/sparrow_version.hpp
@@ -17,10 +17,10 @@
 namespace sparrow
 {
     constexpr int SPARROW_VERSION_MAJOR = 1;
-    constexpr int SPARROW_VERSION_MINOR = 0;
+    constexpr int SPARROW_VERSION_MINOR = 1;
     constexpr int SPARROW_VERSION_PATCH = 0;
 
-    constexpr int SPARROW_BINARY_CURRENT = 9;
+    constexpr int SPARROW_BINARY_CURRENT = 10;
     constexpr int SPARROW_BINARY_REVISION = 0;
-    constexpr int SPARROW_BINARY_AGE = 0;
+    constexpr int SPARROW_BINARY_AGE = 1;
 }
