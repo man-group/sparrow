5426f86af4e560c7b978a6af3a17640fa86dfade
Alexis Placet <alexis.placet@quantstack.net>
2025-10-28 10:14:33 +0100

    wip

--- src/arrow_interface/arrow_schema.cpp
+++ src/arrow_interface/arrow_schema.cpp
@@ -87,8 +87,8 @@ namespace sparrow
         }
 
         target.private_data = new arrow_schema_private_data(
-            std::string(source.format),
-            std::string(source.name),
+            source.format,
+            source.name,
             std::move(metadata_str),
             repeat_view<bool>{true, static_cast<std::size_t>(target.n_children)},
             true
